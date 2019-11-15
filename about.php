1daf121311d7b7187491bfda619922cc7e77f742
talhaamir4130 <talhaamir2014@gmail.com>
2019-11-15 17:11:33 +0500

    Create about.php

---
+++ about.php
@@ -0,0 +1,254 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>About</title>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="scss/main.css">
+    <link rel="stylesheet" href="scss/skin.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="./script/index.js"></script>
+
+</head>
+
+<body id="wrapper">
+
+
+    <header>
+        <nav class="navbar navbar-inverse">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			  </button>
+                    <a class="navbar-brand" href="index.php">
+                        <h1>Lesions Classifier</h1><span>Machine learning & Image Processing</span></a>
+                </div>
+                <div id="navbar" class="collapse navbar-collapse navbar-right">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="index.php">Home</a></li>
+                            <li><a href="about.php">About</a></li>
+                            <li><a href="gallery.php">Gallery</a></li>
+                            <li><a href="login.php">Sign In</a></li>
+                            <li><a href="registration.php">Sign Up</a></li>
+                        </ul>
+                    </div>
+                <!--/.nav-collapse -->
+            </div>
+        </nav>
+        <!--/.nav-ends -->
+    </header>
+    <section id="top_banner">
+        <div class="banner">
+            <div class="inner text-center">
+                <h2>Skin Lesions detection</h2>
+            </div>
+        </div>
+        <div class="page_info">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-8 col-xs-6">
+                        <h4>About</h4>
+                    </div>
+                    <div class="col-md-4 col-sm-4 col-xs-6" style="text-align:right;">Home<span class="sep"> 	/ </span><span class="current"> About</span></div>
+                </div>
+            </div>
+        </div>
+
+        </div>
+    </section>
+
+
+    <section id="about-page-section-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-7 text-align">
+                    <div class="section-heading">
+                        <h2>About <span>Us</span></h2>
+                        <p class="subheading">Skin detection with machine learning</p>
+                    </div>
+                    <p>
+                        Recognizing Skin Disease using Image Processing is a smartphone app that can recognize the skin disease (lesions) and give the appropriate classification for it. The user will open the app and point the camera towards his/her defected skin. Then the user will take an image of his/her skin. Then this image is sent to background processes where all the necessary image processing techniques are applied to extract the desired features for example greyscale, noise removal, edge detection, morphology etc. After the features have been extracted then this information is fed into a trained neural network. The neural network is designed to classify the image according to its machine learning capabilities and the dataset it’s trained on. Then the neural network will generate output and put the image into a class. This class will show us the type of lesions the person has. Then the output will be shown to the user and its classification with other skin lesion categories.
+
+                    </p>
+                </div>
+                <div class="col-xs-12 col-sm-6 col-md-6 col-lg-5">
+                    <img height="" width="auto" src="img/1.jpg" class="attachment-full img-responsive" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="team-member">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 xol-md-12 col-sm-12 col-xs-12">
+                    <div class="section-heading text-center">
+                        <h1>Our <span>Team</span></h1>
+                        <p class="subheading">Our team have highly motivated people who are hard at work in making and improving our skin disease detection system.</p>
+                    </div>
+            		<div class="wpb_column vc_column_container col-md-3 col-sm-6 col-xs-6 block mybox">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class="our-team main-info-below-image">
+                                    <div class="our-team-inner">
+                                        <div class="our-team-image">
+                                            <img src="img/sir_danish.jpg" />
+                                            <div class="qodef-circle-animate"></div>
+                                            <div class="our-team-position-icon">
+                                                <span class="qodef-icon-shortcode circle">
+        			<i class="qodef-icon-simple-line-icon qodef-icon-element fa-wrench fa"></i>
+            </span>
+                                            </div>
+                                            <h6 class="q_team_position">Supervisor</h6>
+                                        </div>
+                                        <div class="our-team-info">
+                                            <div class="our-team-title-holder">
+                                                <h5 class="our-team-name">Dr. Danish Shehzad</h5>
+                                            </div>
+                                            <div class='our-team-text-inner'>
+                                                <div class='our-team-description'>
+                                                    <p>Assistant Professor at FAST-NU CFD</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container col-md-3 col-sm-6 col-xs-6 block mybox">
+                        <div class="vc_column-inner">
+                            <div class="wpb_wrapper">
+                                <div class="our-team main-info-below-image">
+                                    <div class="our-team-inner">
+                                        <div class="our-team-image">
+                                            <img src="img/talha.jpg" />
+                                            <div class="qodef-circle-animate"></div>
+                                            <div class="our-team-position-icon">
+                                                <span class="qodef-icon-shortcode circle">
+        				<i class="qodef-icon-simple-line-icon fa fa-cog qodef-icon-element"></i>
+    					</span>
+                                            </div>
+                                            <h6 class="q_team_position">Team member</h6>
+                                        </div>
+                                        <div class="our-team-info">
+                                            <div class="our-team-title-holder">
+                                                <h5 class="our-team-name">Talha Amir</h5>
+                                            </div>
+                                            <div class='our-team-text-inner'>
+                                                <div class='our-team-description'>
+                                                    <p>Final year student at FAST-NU CFD</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container col-md-3 col-sm-6 col-xs-6 block mybox">
+                        <div class="vc_column-inner ">
+                            <div class="wpb_wrapper">
+                                <div class="our-team main-info-below-image">
+                                    <div class="our-team-inner">
+                                        <div class="our-team-image">
+                                            <img src="img/tahir.jpg" />
+                                            <div class="qodef-circle-animate"></div>
+                                            <div class="our-team-position-icon">
+                                                <span class="qodef-icon-shortcode circle">        
+        					<i class="qodef-icon-simple-line-icon fa fa-cog qodef-icon-element"></i>
+            		</span>
+                                            </div>
+                                            <h6 class="q_team_position">Team member</h6>
+                                        </div>
+                                        <div class="our-team-info">
+                                            <div class="our-team-title-holder">
+                                                <h5 class="our-team-name">Tahir Ahmed</h5>
+                                            </div>
+                                            <div class='our-team-text-inner'>
+                                                <div class='our-team-description'>
+                                                    <p>Final year student at FAST-NU CFD</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="wpb_column vc_column_container col-md-3 col-sm-6 col-xs-6 block mybox">
+                        <div class="vc_column-inner ">
+                            <div class="wpb_wrapper">
+                                <div class="our-team main-info-below-image">
+                                    <div class="our-team-inner">
+                                        <div class="our-team-image">
+                                            <img src="img/uzair.jpg" />
+                                            <div class="qodef-circle-animate"></div>
+                                            <div class="our-team-position-icon">
+                                                <span class="qodef-icon-shortcode circle ">
+						        <i class="qodef-icon-simple-line-icon fa fa-cog qodef-icon-element" ></i>
+			            </span>
+                                            </div>
+                                            <h6 class="q_team_position">Team member</h6>
+                                        </div>
+                                        <div class="our-team-info">
+                                            <div class="our-team-title-holder">
+                                                <h5 class="our-team-name">Uzair Sadiq</h5>
+                                            </div>
+                                            <div class='our-team-text-inner'>
+                                                <div class='our-team-description'>
+                                                    <p>Final year student at FAST-NU CFD</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                
+
+                <div class="col-md-3 col-sm-3 col-xs-12 block">
+                    <div class="footer-block">
+                        <h4>Useful Links</h4>
+                        <hr/>
+                       <ul class="footer-links">
+                            <li><a href="about.php">About Us</a></li>
+                            <li><a href="gallery.php">Portfolio</a></li>
+                            <li><a href="login.php">Sign In</a></li>
+                            <li><a href="registration.php">Sign Up</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                
+            </div>
+        </div>
+
+
+    </section>
+
+
+
+</body>
+
+</html>
